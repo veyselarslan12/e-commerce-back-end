55bb69400263545d68037bf04ebd46b2a39bdac1
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-05-25 23:48:23 -0500

    model-index.js foreign key and through

--- models/index.js
+++ models/index.js
@@ -6,24 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+  foreignKey: ''
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: ''
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-
+  foreignKey: ''
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  
+  foreignKey: ''
 })
 
 
