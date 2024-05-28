1789f080385c41fe1e3ffe3295ef9f87eff84b19
Veysel Arslan <veysel.arslan1288@gmail.com>
2024-05-27 22:35:09 -0500

    tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,89 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  // findAll()
+  try {
+    const tags = await Tag.findAll({
+      include: [
+        {
+          model: Product
+        }
+      ]
+    })
+    res.json(tags)
+
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Error getting tags.')
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  // findByPk()
+  const { id } = req.params
+  try {
+    const tag = await Tag.findByPk(id, {
+      include: [
+        {
+          model: Product
+        }
+      ]
+    })
+    res.json(tag)
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Error getting tag.')
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  // create()
+  try {
+    const newTag = await Tag.create(req.body)
+    res.json(newTag)
+
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Error creating tag.')
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  // update()
+  const { id } = req.params
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: { id }
+    })
+    res.json(updateTag)
+
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Error updating tag.')
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  // destroy()
+  const { id } = req.params
+  try {
+    const deleteTag = await Tag.destroy({
+      where: { id }
+    })
+    res.json(deleteTag)
+    
+  } catch(err) {
+    console.log(err)
+    res.status(500).send('Error deleting tag.')
+  }
 });
 
 module.exports = router;
